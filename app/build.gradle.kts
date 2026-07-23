bf631b5cfadc275d6508cda189b735b845f65a54
okra <64577780+akilxasp@users.noreply.github.com>
2026-07-24 00:59:03 +0530

    Add copyable Glyph debug dump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dev.akil.rapidoglyph"
         minSdk = 34
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -42,4 +42,3 @@ dependencies {
     implementation(files("libs/glyph-matrix-sdk-2.0.aar"))
     testImplementation("junit:junit:4.13.2")
 }
-
